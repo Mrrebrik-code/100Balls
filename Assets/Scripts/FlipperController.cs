f127420f326d2b797905df09c211e732e0937ec2
Sanan Beybudov <jbmashina@gmail.com>
2022-02-22 15:54:58 +0400

    added spawner

--- Assets/Scripts/FlipperController.cs
+++ Assets/Scripts/FlipperController.cs
@@ -7,22 +7,25 @@ public class FlipperController : MonoBehaviour
     [SerializeField] private Transform leftFlipper;
     [SerializeField] private Transform rightFlipper;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            leftFlipper.localEulerAngles = new Vector3(leftFlipper.localEulerAngles.x, leftFlipper.localEulerAngles.y, -90.0f);
-            rightFlipper.localEulerAngles = new Vector3(rightFlipper.localEulerAngles.x, rightFlipper.localEulerAngles.y, 90.0f);
+            OpenFlippers();
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            leftFlipper.localEulerAngles = Vector3.zero;
-            rightFlipper.localEulerAngles = Vector3.zero;
+            CloseFlippers();
         }
+    }
+    private void OpenFlippers()
+    {
+        leftFlipper.localEulerAngles = new Vector3(leftFlipper.localEulerAngles.x, leftFlipper.localEulerAngles.y, -90.0f);
+        rightFlipper.localEulerAngles = new Vector3(rightFlipper.localEulerAngles.x, rightFlipper.localEulerAngles.y, 90.0f);
+    }
+    private void CloseFlippers()
+    {
+        leftFlipper.localEulerAngles = Vector3.zero;
+        rightFlipper.localEulerAngles = Vector3.zero;
     }
 }
---
+++ Assets/Scripts/IceSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform leftSpawnPoint;
+    [SerializeField] private Transform rightSpawnPoint;
+    [SerializeField] private GameObject spawnObject;
+    private enum SpawnFlipper
+    {
+        Left,
+        Right
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            StartCoroutine(Enumerator());
+        }
+    }
+    private void SpawnObject()
+    {
+        var randomNumber = Random.Range(-1, 1);
+        var flipper = (SpawnFlipper)randomNumber;
+
+        if (flipper == SpawnFlipper.Left) Instantiate(spawnObject, leftSpawnPoint);
+        else Instantiate(spawnObject, rightSpawnPoint);
+    }
+    IEnumerator Enumerator()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            SpawnObject();
+        }
+    }
+}
